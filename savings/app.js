7d674b210541601d33b67da0dc324c1c5f407e6b
kklein <kklein@users.noreply.github.com>
2023-05-05 08:12:36 +0000

    deploy: 7f05282212deea7ff72ae4eab0ff5334fad722d9

--- savings/app.js
+++ savings/app.js
@@ -1,3 +1,27 @@
+function readField(field_id) {
+  return parseFloat(document.getElementById(field_id).value);
+}
+
+function readFields() {
+  return {
+    savingsGoal: readField("savings-goal"),
+    roi: readField("roi"),
+    annualSavings: readField("annual-savings"),
+    savingsIncrease: readField("savings-increase"),
+    annualSpending: readField("annual-spending"),
+  };
+}
+
+function newtonRaphson(f, fDer, x0, nIter) {
+  let x = x0;
+  for (let i = 0; i < nIter; i++) {
+    const y = f(x);
+    const dy = fDer(x);
+    x = x - y / dy;
+  }
+  return x;
+}
+
 function savings(nYears, roi, savingsIncrease, annualSavings) {
   const fraction = Math.pow((1 + savingsIncrease) / (1 + roi), nYears);
   return (
@@ -11,7 +35,7 @@ function savingsDerivative(nYears, roi, savingsIncrease, annualSavings) {
 }
 
 function cumulativeSpending(nYears, roi, extraSpending) {
-    return extraSpending * Math.pow((1 + roi), nYears)
+  return extraSpending * Math.pow(1 + roi, nYears);
 }
 
 function totalRoot(
@@ -42,110 +66,110 @@ function totalDerivative(
   );
 }
 
-function newtonRaphson(f, fDer, x0, nIter) {
-  let x = x0;
-  for (let i = 0; i < nIter; i++) {
-    const y = f(x);
-    const dy = fDer(x);
-    x = x - y / dy;
-  }
-  return x;
-}
-
-function readField(field_id) {
-  return parseFloat(document.getElementById(field_id).value);
-}
-
-function readFields() {
-  return {
-    savingsGoal: readField("savings-goal"),
-    roi: readField("roi"),
-    annualSavings: readField("annual-savings"),
-      savingsIncrease: readField("savings-increase"),
-      annualSpending: readField("annual-spending")
-  };
-}
-
 function savingsBreak(
   nYears,
   roi,
   savingsIncrease,
-    annualSavings,
-    annualSpending,
-    durationBreak,
-    timeOfBreak,
+  annualSavings,
+  annualSpending,
+  durationBreak,
+  timeOfBreak
 ) {
-    const hFirst = timeOfBreak;
-    const hSecond = nYears - timeOfBreak - durationBreak;
-    const preBreakSavings = savings(hFirst, roi, savingsIncrease, annualSavings);
-    const costOfLiving = cumulativeSpending(hSecond, roi, annualSpending * durationBreak);
-    const passiveIncome = cumulativeSpending(hSecond + durationBreak, roi, preBreakSavings) - preBreakSavings;
-    const newAnnualSavings = cumulativeSpending(hFirst, savingsIncrease, annualSavings);
-    const postBreakSavings = savings(hSecond, roi, savingsIncrease, newAnnualSavings);
-    return preBreakSavings + passiveIncome - costOfLiving + postBreakSavings;
+  const hFirst = timeOfBreak;
+  const hSecond = nYears - timeOfBreak - durationBreak;
+  const preBreakSavings = savings(hFirst, roi, savingsIncrease, annualSavings);
+  const costOfLiving = cumulativeSpending(
+    hSecond,
+    roi,
+    annualSpending * durationBreak
+  );
+  const passiveIncome =
+    cumulativeSpending(hSecond + durationBreak, roi, preBreakSavings) -
+    preBreakSavings;
+  const newAnnualSavings = cumulativeSpending(
+    hFirst,
+    savingsIncrease,
+    annualSavings
+  );
+  const postBreakSavings = savings(
+    hSecond,
+    roi,
+    savingsIncrease,
+    newAnnualSavings
+  );
+  return preBreakSavings + passiveIncome - costOfLiving + postBreakSavings;
 }
 
 function savingsBreakDer(
   nYears,
   roi,
   savingsIncrease,
-    annualSavings,
-    annualSpending,
-    durationBreak,
-    timeOfBreak,
+  annualSavings,
+  annualSpending,
+  durationBreak,
+  timeOfBreak
 ) {
-    const hFirst = timeOfBreak;
-    const hSecond = nYears - timeOfBreak - durationBreak;
-    const newAnnualSavings = cumulativeSpending(hFirst, savingsIncrease, annualSavings);
-    const costOfLivingDer = cumulativeSpending(hSecond, roi, annualSpending);
-    const postBreakSavingsDer = - savings(hSecond, roi, savingsIncrease, newAnnualSavings);
-    return costOfLivingDer + postBreakSavingsDer
+  const hFirst = timeOfBreak;
+  const hSecond = nYears - timeOfBreak - durationBreak;
+  const newAnnualSavings = cumulativeSpending(
+    hFirst,
+    savingsIncrease,
+    annualSavings
+  );
+  const costOfLivingDer = cumulativeSpending(hSecond, roi, annualSpending);
+  const postBreakSavingsDer = -savings(
+    hSecond,
+    roi,
+    savingsIncrease,
+    newAnnualSavings
+  );
+  return costOfLivingDer + postBreakSavingsDer;
 }
 
 function findBreakDuration(nYears, timeOfBreak, nIter) {
-    fields = readFields();
-    const f = (x) =>
-	  savings(nYears, fields.roi, fields.savingsIncrease, fields.annualSavings)
-	  - savingsBreak(
-	      nYears,
-	      fields.roi,
-	      fields.savingsIncrease,
-	      fields.annualSavings,
-	      fields.annualSpending,
-	      x,
-	      timeOfBreak,
-	  )
-	  - cumulativeSpending(nYears, fields.roi, 100)
-    const fDer = (x) =>
-	  - savingsBreakDer(
-	      nYears,
-	      fields.roi,
-	      fields.savingsIncrease,
-	      fields.annualSavings,
-	      fields.annualSpending,
-	      x,
-	      timeOfBreak,
-	  )
-    const x0 = 1/365;
-    const result = newtonRaphson(f, fDer, x0, nIter);
-    return result
+  fields = readFields();
+  const f = (x) =>
+    savings(nYears, fields.roi, fields.savingsIncrease, fields.annualSavings) -
+    savingsBreak(
+      nYears,
+      fields.roi,
+      fields.savingsIncrease,
+      fields.annualSavings,
+      fields.annualSpending,
+      x,
+      timeOfBreak
+    ) -
+    cumulativeSpending(nYears, fields.roi, 100);
+  const fDer = (x) =>
+    -savingsBreakDer(
+      nYears,
+      fields.roi,
+      fields.savingsIncrease,
+      fields.annualSavings,
+      fields.annualSpending,
+      x,
+      timeOfBreak
+    );
+  const x0 = 1 / 365;
+  const result = newtonRaphson(f, fDer, x0, nIter);
+  return result;
 }
 
 function setupSlider(nYears) {
+  const timeSlider = document.getElementById("time-slider");
+  timeSlider.min = 0;
+  timeSlider.max = nYears;
+  timeSlider.value = nYears / 2;
+  const update = function () {
     const timeSlider = document.getElementById("time-slider");
-    timeSlider.min = 0;
-    timeSlider.max = nYears;
-    timeSlider.value = nYears / 2;
-    const update = function() {
-	const timeSlider = document.getElementById("time-slider");
-	const timeOfBreak = parseInt(timeSlider.value)
-	const breakDuration = findBreakDuration(nYears, timeOfBreak, 100)
-	const breakDurationText = (breakDuration * 365).toFixed(2);
-	const resultString = `You may take a break of ${breakDurationText} days after ${timeSlider.value} years while still hitting your savings goal.`;
-	document.getElementById("result-break").textContent = `${resultString}`;
-    }
-    update()
-    timeSlider.oninput = update
+    const timeOfBreak = parseInt(timeSlider.value);
+    const breakDuration = findBreakDuration(nYears, timeOfBreak, 100);
+    const breakDurationText = (breakDuration * 365).toFixed(2);
+    const resultString = `You may take a break of ${breakDurationText} days after ${timeSlider.value} years while still hitting your savings goal.`;
+    document.getElementById("result-break").textContent = `${resultString}`;
+  };
+  update();
+  timeSlider.oninput = update;
 }
 
 function findSavingsDuration(nIter) {
@@ -192,20 +216,28 @@ function findSavingsExtraDuration(nYears, extraSpending, nIter) {
 }
 
 function calculate() {
-    const nIter = 100;
+  const nIter = 100;
+  const fields = readFields();
+  hint = document.getElementById("calculate-hint");
+  if (fields.roi == fields.savingsIncrease) {
+    hint.hidden = false;
+    document.getElementById("results").hidden = true;
+    return;
+  }
 
-    const nYears = findSavingsDuration(nIter)
-    const nYearsText = `It will take approximately ${nYears.toFixed(
-      2
-    )} years to hit your savings goal.`;
-    document.getElementById("result-duration").textContent = nYearsText;
+  const nYears = findSavingsDuration(nIter);
+  const nYearsText = `It will take approximately ${nYears.toFixed(
+    2
+  )} years to hit your savings goal.`;
+  document.getElementById("result-duration").textContent = nYearsText;
 
-    const nYearsExtra = findSavingsExtraDuration(nYears, 100, nIter)
-    const nDaysExtra = ((nYearsExtra - nYears) * 365).toFixed(2);
-    const nDaysExtraText = `Spending an extra 100 of your monetary currency right now will delay reaching your savings goal, i.e. 'cost you', approximately ${nDaysExtra} days.`;
-    document.getElementById("result-money").textContent = nDaysExtraText;
+  const nYearsExtra = findSavingsExtraDuration(nYears, 100, nIter);
+  const nDaysExtra = ((nYearsExtra - nYears) * 365).toFixed(2);
+  const nDaysExtraText = `Spending an extra 100 of your monetary currency right now will delay reaching your savings goal, i.e. 'cost you', approximately ${nDaysExtra} days.`;
+  document.getElementById("result-money").textContent = nDaysExtraText;
 
-    setupSlider(nYears)
+  setupSlider(nYears);
 
-    document.getElementById("results").hidden = false;
+  hint.hidden = true;
+  document.getElementById("results").hidden = false;
 }
